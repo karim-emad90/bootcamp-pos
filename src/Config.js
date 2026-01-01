24d32983eb50e8ed04ae397ccbee0e6175520334
karim-emad90 <karim.sharkawy90@gmail.com>
2026-01-01 11:40:53 +0200

    se

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
 // الرابط الكامل للـ Strapi المرفوع على Render
-export const API_BASE_URL = "https://5578927f2da4.ngrok-free.app";
+export const API_BASE_URL = "https://861c7e41cc3e.ngrok-free.app";
